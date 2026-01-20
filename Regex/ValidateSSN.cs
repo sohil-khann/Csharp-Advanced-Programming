7e6cefd0de176c2134f89c86c98112f2531b8f39
sohil-khann <khaa40408@gmail.com>
2026-01-20 16:54:03 +0530

    [Sohil khan] Add. ValidateSSN

---
+++ Regex/ValidateSSN.cs
@@ -0,0 +1,24 @@
+/*15. Validate a Social Security Number (SSN) 
+ Example Input: "My SSN is 123-45-6789." 
+ Expected Output: 
+ ● ✅ "123-45-6789" is valid 
+ ● ❌ "123456789" is invalid */
+
+using System.Text.RegularExpressions;
+
+public class ValidateSSN
+{
+    public static void Validate(string ssn)
+    {
+        string pattern = @"^\d{3}-\d{2}-\d{4}$";
+
+        if (Regex.IsMatch(ssn, pattern))
+        {
+            Console.WriteLine($"✅ \"{ssn}\" is valid");
+        }
+        else
+        {
+            Console.WriteLine($"❌ \"{ssn}\" is invalid");
+        }
+    }
+}
