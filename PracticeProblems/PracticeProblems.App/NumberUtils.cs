f12d17dc3a347acfbba05a98130d46047e8cf6aa
sohil-khann <khaa40408@gmail.com>
2026-01-22 12:46:31 +0530

    [Sohil khan] update. solution of NumberUtils

---
+++ PracticeProblems/PracticeProblems.App/NumberUtils.cs
@@ -0,0 +1,9 @@
+namespace PracticeProblems.App;
+
+public class NumberUtils
+{
+    public static bool IsEven(int number)// Method to check if a number is even
+    {
+        return number % 2 == 0;
+    }
+}
